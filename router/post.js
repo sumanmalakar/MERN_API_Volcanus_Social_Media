a2781e5041c7b38d8bbdc9732c9656dba19d960a
“sumanmalakar” <malakars041@gmail.com>
2024-01-15 17:52:39 +0530

    API completed

---
+++ router/post.js
@@ -0,0 +1,35 @@
+import express from "express";
+import {
+  addPost,
+  getPosts,
+  getPostById,
+  updatePost,
+  deletePost,
+  likePostById,
+  commentPostById,
+  getCommentByPostId
+  // findCommentByPostId,
+} from "../controllers/post.js";
+
+import { Authenticate } from "../middlewares/auth.js";
+
+export const postRouter = express.Router();
+
+postRouter.post("/addpost", Authenticate, addPost);
+
+postRouter.get("/posts", getPosts);
+
+postRouter.get("/post/:id", Authenticate, getPostById);
+
+postRouter.put("/post/:id", Authenticate, updatePost);
+
+postRouter.delete("/post/:id", Authenticate, deletePost);
+ 
+postRouter.post("/post/like/:id",Authenticate, likePostById);
+
+postRouter.post("/post/comment/:id",Authenticate, commentPostById);
+
+postRouter.get("/post/comment/:id", Authenticate, getCommentByPostId);
+
+
+
---
+++ server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import mongoose from "mongoose";
+import { config } from "dotenv";
+import userRouter from "./router/user.js";
+import { postRouter } from "./router/post.js";
+import bodyParser from "express";
+
+const app = express();
+
+// creating config file
+config({ path: ".env" });
+
+app.use(bodyParser.json());
+
+// userRouter
+app.use("/api", userRouter);
+
+// postRouter
+app.use("/api", postRouter);
+
+// DB Connection
+mongoose
+  .connect(process.env.MONGO_URL, {
+    dbName: "Blogging_MERN_Volcanus",
+  })
+  .then(() => console.log("MongoDB is Connected..!"));
+
+// Server Setup
+const port = process.env.PORT;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
